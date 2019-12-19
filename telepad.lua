1804ff32592a767a12f1b84c310b09062d67589c
BrightYC <sawerty222@gmail.com>
2019-12-19 04:18:02 +0300

    Update telepad.lua

--- telepad.lua
+++ telepad.lua
@@ -6,145 +6,170 @@ local inv, robot, redstone, chat = component.inventory_controller, component.rob
 local size = robot.inventorySize()
 local teleports, oldDestination
 
-local symbol, side = "?", 3
-local whiteList = {}
+local symbol, side, bufferSide = "?", 1, 3 --bufferSide - для версий выше 1.7
+local version = "paper" --Версия работы - selector для 1.7, paper - для версией выше 1.7
+local whiteList = {"Vasya", "Petya"} --Белый список игроков, другие игроки не смогут использовать телепорт
 
 local function sort(a, b)
-	return a.label < b.label
+    return a.label < b.label
 end
 
 local function scan()
-	teleports = {}
-	local page, counter = 1, 0
+    teleports = {}
+    local page, counter = 1, 0
 
-	for slot = 1, size do 
-		local item = inv.getStackInInternalSlot(slot)
+    for slot = 1, size do 
+        local item = inv.getStackInInternalSlot(slot)
 
-		if item then
-			if item.name == "EnderIO:itemCoordSelector" or item.name == "enderio:item_location_printout" then
-				local teleport = #teleports + 1
-				teleports[teleport] = {label = item.label, slot = slot}
-				teleports[teleport].title = "§f'§a" .. teleports[teleport].label .. "§f'"
-				teleports[teleport].page = page
-				counter = counter + 1
-				if counter == 8 then
-					page, counter = page + 1, 0
-				end
-			end
-		end
-	end
+        if item and (item.name == "EnderIO:itemCoordSelector" or item.name == "enderio:item_location_printout") then
+            local teleport = #teleports + 1
+            teleports[teleport] = {label = item.label, slot = slot}
+            teleports[teleport].title = "§f'§a" .. teleports[teleport].label .. "§f'"
+            teleports[teleport].page = page
+            counter = counter + 1
+            if counter == 8 then
+                page, counter = page + 1, 0
+            end
+        end
+    end
 
-	teleports.pages = math.ceil(#teleports / 8)
-	table.sort(teleports, sort)
+    teleports.pages = math.ceil(#teleports / 8)
+    table.sort(teleports, sort)
 
-	if #teleports == 0 then
-		print("Ни одной точки не найдено!")
-		os.exit()
-	else
-		print("Точек найдено: " .. #teleports)
-	end
+    if #teleports == 0 then
+        print("Ни одной точки не найдено!")
+        os.exit()
+    else
+        print("Точек найдено: " .. #teleports)
+    end
 end
 
 local function help()
-	chat.say("§7Телепорт на точку: §c?tp §bточка§7.")
-	chat.say("§7Список точек: §c?list §bстраница§7.")
-	chat.say("§7Обновление точек: §c?update§7.")
+    chat.say("§7Телепорт на точку: §c?tp §bточка§7.")
+    chat.say("§7Список точек: §c?list §bстраница§7.")
+    chat.say("§7Обновление точек: §c?update§7.")
 end
 
-local function manualTeleport(teleport)
-	chat.say("§7Подготовка к телепортации на '§a" .. teleports[teleport].label .. "§7'...")
-	robot.select(teleports[teleport].slot)
-	inv.equip()
-	robot.use(side, true)
-	inv.equip()
-	robot.select(1)
-	redstone.setOutput(side, 15)
-	redstone.setOutput(side, 0)
+local function activate()
+    redstone.setOutput(side, 15)
+    redstone.setOutput(side, 0)
 end
 
-local function teleport(destination)
-	if destination then
-		destination = unicode.lower(destination)
-		local found
+local function teleportation(teleport)
+    chat.say("§7Подготовка к телепортации на '§a" .. teleports[teleport].label .. "§7'...")
+    robot.select(teleports[teleport].slot)
 
-		for teleport = 1, #teleports do 
-			if unicode.sub(unicode.lower(teleports[teleport].label), 1, unicode.len(destination)) == destination then 
-				manualTeleport(teleport)
-				oldDestination = teleport
-				found = true
-				break
-			end
-		end
+    if version == "selector" then
+        inv.equip()
+        robot.use(side, true)
+        activate()
+        inv.equip()
+    elseif version == "paper" then
+        robot.drop(side)
+        activate()
+        counter = 0
 
-		if not found then 
-			chat.say("§cТочки " .. destination ..  " не существует!")
-			help()
-		end
-	elseif oldDestination then 
-		manualTeleport(oldDestination)
-	else
-		help()
-	end
+        while true do 
+            local item = inv.getStackInSlot(bufferSide, 1)
+            counter = counter + 1
+
+            if item and (item.name == "enderio:item_location_printout") then --как же с этого горит, ну почему нельзя из телепорта достать распечатку?!
+                robot.suck(bufferSide)
+                break
+            end
+
+            print("Попытка поиска распечатки #" .. counter)
+            os.sleep(0)
+        end
+    end
+
+    robot.select(1)
 end
 
-local function list(page)	
-	if page then 
-		page = tonumber(page)
+local function findDestination(destination)
+    if destination then
+        destination = unicode.lower(destination)
+        local found
 
-		if not page then 
-			help()
-		else
-			page = math.floor(page)
-		end
-	else
-		page = 1
-	end
+        for teleport = 1, #teleports do 
+            if unicode.sub(unicode.lower(teleports[teleport].label), 1, unicode.len(destination)) == destination then 
+                teleportation(teleport)
+                oldDestination = teleport
+                found = true
+                break
+            end
+        end
 
-	if page then
-		if page > teleports.pages then 
-			chat.say("§cВсего " .. teleports.pages .. " страниц.")
-		else
-			chat.say("§6§lТочки - Страница " .. page .. "/" .. teleports.pages)
-			for teleport = 1, #teleports do
-				if teleports[teleport].page == page then
-					chat.say(teleports[teleport].title)
-				end
-			end
-		end
-	end
+        if not found then 
+            chat.say("§cТочки " .. destination ..  " не существует!")
+            help()
+        end
+    elseif oldDestination then 
+        teleportation(oldDestination)
+    else
+        help()
+    end
+end
+
+local function list(page)   
+    if page then 
+        page = tonumber(page)
+
+        if not page then 
+            help()
+        else
+            page = math.floor(page)
+        end
+    else
+        page = 1
+    end
+
+    if page then
+        if page > teleports.pages then 
+            chat.say("§cВсего " .. teleports.pages .. " страниц.")
+        else
+            chat.say("§6§lТочки - Страница " .. page .. "/" .. teleports.pages)
+            for teleport = 1, #teleports do
+                if teleports[teleport].page == page then
+                    chat.say(teleports[teleport].title)
+                end
+            end
+        end
+    end
 end
 
 local cmd = {
-	help = help,
-	tp = teleport,
-	list = list,
-	update = scan
+    help = help,
+    tp = findDestination,
+    list = list,
+    update = scan
 }
 
 for user = 1, #whiteList do 
     whiteList[whiteList[user]], whiteList[user] = true, nil
 end 
+robot.select(1)
 chat.setName("§eTelepad§7§o")
 scan()
 
 while true do 
-	local evt = {event.pull()}
+    local evt = {event.pull()}
 
-	if evt[1] == "chat_message" and whiteList[evt[3]] then
-		chat.setName("§eTelepad§7§o")
-		if evt[4]:sub(1, 1) == symbol then
-			local message = evt[4]:sub(2, #evt[4])
-			local args = {}
+    if evt[1] == "chat_message" and whiteList[evt[3]] then
+        chat.setName("§eTelepad§7§o")
+        if evt[4]:sub(1, 1) == symbol then
+            local message = evt[4]:sub(2, #evt[4])
+            local args = {}
 
-			for part in message:gsub("^(.-)%s*$", "%1"):gmatch("%s*([^%s]+)") do 
-				args[#args+1]=("%q"):format(part):gsub('"', '')
-			end 
+            for part in message:gsub("^(.-)%s*$", "%1"):gmatch("%s*([^%s]+)") do 
+                args[#args+1]=("%q"):format(part):gsub('"', '')
+            end 
 
-			if cmd[args[1]] then 
-				cmd[args[1]](table.unpack(args, 2, #args))
-			else
-				help()
-			end
-		end
-	end
+            if cmd[args[1]] then 
+                cmd[args[1]](table.unpack(args, 2, #args))
+            else
+                help()
+            end
+        end
+    end
 end
