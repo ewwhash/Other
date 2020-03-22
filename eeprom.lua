513d6fbfd9591c22a54dcb0ec74f30a74e2513ac
BrightYC <sawerty222@gmail.com>
2020-03-22 23:30:51 +0300

    .

--- eeprom.lua
+++ eeprom.lua
@@ -1,8 +1,6 @@
-@ -1,211 +1 @@
-
-local bootFiles, Component, Computer, Table, Math, Background, Foreground, white, bootCandidates, False, gpuAndScreen, width, height, centerY = {
+local bootFiles, Component, Computer, Table, Math, Background, Foreground, white, keyDown, bootCandidates, False, gpuAndScreen, width, height, centerY = {
     "/init.lua"
-}, component, computer, table, math, 0x002b36, 0x8cb9c5, 0xffffff
+}, component, computer, table, math, 0x002b36, 0x8cb9c5, 0xffffff, "key_down"
 
 local componentProxy, componentList, computerPullSignal, computerUptime, mathHuge, mathCeil, tableInsert = Component.proxy, Component.list, Computer.pullSignal, computer.uptime, Math.huge, Math.ceil, Table.insert
 
@@ -20,7 +18,7 @@ if gpuSetBackground and screen then
     gpu.bind((componentList"sc"()))
     gpuAndScreen, width, height = 1, gpu.maxResolution()
     centerY = height / 2
-    gpu.setPalleteColor(9, 0x002b36)
+    gpu.setPaletteColor(9, 0x002b36)
     gpu.setResolution(width, height)
 end
 
@@ -91,20 +89,20 @@ function(x, y, width, height, symbol, background, foreground)
     gpuSetBackground(background or Background)
     gpuSetForeground(foreground or Foreground)
     gpu.fill(x, y, width, height, symbol)
-end
+end,
 
 function(len)
     return mathCeil(width / 2 - len / 2)
 end
 
-local clear, center, sleep, bootFrom = 
+local clear, center, sleep, bootFrom, elementsLen = 
 
 function()
     fill(1, 1, width, height, " ", 0x002b36)
 end,
 
 function(y, str, background, foreground)
-    set(getCenterX(unicode.len(str) / 2), y, str, background, foreground)
+    set(getCenterX(unicode.len(str)), y, str, background, foreground)
 end,
 
 function(timeout, breakCode, onBreak)
@@ -112,17 +110,28 @@ function(timeout, breakCode, onBreak)
     repeat
         local signal = {computerPullSignal(deadline - computerUptime())}
 
-        if signal[1] == "key_down" and (breakCode == 0 or signal[4] == breakCode) then
+        if signal[1] == keyDown and (breakCode == 0 or signal[4] == breakCode) then
             if onBreak then
                 onBreak()
             end
-            break
+            return 1
         end
     until computerUptime() >= deadline
 end,
 
 function(proxy, file, alreadyBooting)
     return("Boot%s %s from %s (%s)"):format(alreadyBooting and "ing" or "", file, proxy.getLabel() or "undefined", proxy.address)
+end,
+
+function(elements)
+    local allLen = 0
+
+    for i = 1, #elements do
+        local len = unicode.len(elements[i].t)
+        allLen, elements[i].l = allLen + len + (i == #elements and 0 or 8), len
+    end
+
+    return allLen
 end
 
 local function status(str, title, wait, breakCode, onBreak)
@@ -145,7 +154,7 @@ local function status(str, title, wait, breakCode, onBreak)
             y = y + 1
         end
 
-        sleep(wait, breakCode, onBreak)
+        return sleep(wait, breakCode, onBreak)
     end
 end
 
@@ -157,10 +166,10 @@ local function Error(err)
     end
 end
 
-local boot, addBootCandidate =
+local boot, addBootCandidate, menu =
 
-function(proxy, file, prettyViewFile) -- boot()
-    status(bootFrom(proxy, prettyViewFile))
+function(proxy, file, prettyViewFile)
+    status(bootFrom(proxy, prettyViewFile, true), False, .5)
 
     if eepromGetData() ~= proxy.address then
         eepromSetData(proxy.address)
@@ -175,7 +184,7 @@ function(proxy, file, prettyViewFile) -- boot()
     end
 end,
 
-function(address) -- addBootCandidate()
+function(address)
     local proxy = select(2, pcall(componentProxy, address))
 
     if proxy then
@@ -187,6 +196,59 @@ function(address) -- addBootCandidate()
             end
         end
     end
+end,
+
+function(elements, y, actionOnDraw, actionOnArrowKeyUp, actionOnArrowKeyDown, actionOnF5)
+    local globalX, selected, action = getCenterX(elementsLen(elements)), 1,
+
+    function(checkAction, Self)
+        if checkAction and type(checkAction) == "function" then
+            checkAction(Self)
+        end
+    end
+
+    return {
+        d = function()
+            local x = globalX
+            clear()
+
+            for i = 1, #elements do
+                local selectedItem = i == selected and 0x292929
+
+                if selectedItem then
+                    fill(x - 3, y, elements[i].l + 6, 1, " ", selectedItem)
+                end
+
+                set(x, y, elements[i].t, selectedItem)
+                x = x + elements[i].l + 8
+            end
+
+            action(actionOnDraw)
+        end,
+        l = function(Self)
+            while 1 do
+                local signal = {computerPullSignal()}
+
+                if signal[1] == keyDown then
+                    if signal[4] == 203 and selected > 1 then
+                        selected = selected - 1
+                        Self:d()
+                    elseif signal[4] == 205 and selected < #elements then
+                        selected = selected + 1
+                        Self:d()
+                    elseif signal[4] == 200 then
+                        action(actionOnArrowKeyUp, Self)
+                    elseif signal[4] == 208 then
+                        action(actionOnArrowKeyDown, Self)
+                    elseif signal[4] == 63 then
+                        action(actionOnF5, Self)
+                    elseif signal[4] == 28 then
+                        action(elements[selected].a, Self)
+                    end
+                end
+            end
+        end
+    }
 end
 
 local function updateCandidates()
@@ -201,7 +263,16 @@ local function updateCandidates()
 end
 
 updateCandidates()
-clear()
+if status("Press F9 to stay in bootloader", False, .8, 67) then
+    local main = menu({
+        {t = "Power Off", a = computerShutdown},
+        {t = "Settings"},
+        {t = "Shell"},
+        {t = "Recovery"}
+    }, centerY + 5, function() center(height, "Use ← ↑ → keys to move cursor; Enter to do something; F5 to refresh") end, False, False, function(Self) updateCandidates() Self:d() end)
+    main:d()
+    main:l()
+end
 
 if #bootCandidates >= 1 then
     boot(bootCandidates[1][1], bootCandidates[1][2], bootCandidates[1][3])
